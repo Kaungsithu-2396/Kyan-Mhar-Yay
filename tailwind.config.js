6dc24c54da333959a223272060e1b1bb4e4947a7
Kaung Si Thu <kaungsithu@Si-Thus-MacBook-Pro.local>
2022-07-31 23:51:51 +0630

    fourth commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js,php}"],
+  content: ["./public/**/*.{html,js,php}"],
   theme: {
     extend: {
       fontFamily: {
