6baea538985df350cab3399e17d0db1426b0d54e
Kaung Si Thu <kaungsithu@Si-Thus-MacBook-Pro.local>
2022-07-31 22:28:59 +0630

    first commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+let nav= document.querySelector('nav');
+let btn=document.getElementById('btn-nav');
+let topNav=document.getElementById('topNav');
+let select=document.querySelector('.select-accordion');
+let contentSection=document.querySelectorAll('.content-section');
+
+btn.addEventListener('click',function(){
+    nav.classList.toggle('show-nav');
+})
+
+
+
+
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,php}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Nunito', 'sans-serif'],
+      },
+      container:{
+        center:true,
+        // padding:'2rem',
+      },
+    
+     },
+  
+    
+
+  },
+  plugins: [],
+}
